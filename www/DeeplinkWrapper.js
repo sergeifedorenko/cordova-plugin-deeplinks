c1e18bd5d852205929cf99e910e50c92f35a488a
dayan07 <diana-lobach@yandex.ru>
2018-06-21 13:18:29 +0300

    Add js method getCordovaURIFRomIntent

--- www/DeeplinkWrapper.js
+++ www/DeeplinkWrapper.js
@@ -1,5 +1,27 @@
-var exec = require('cordova/exec');
+function DeeplinkWrapper() {
+    'use strict';
+}
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'DeeplinkWrapper', 'coolMethod', [arg0]);
+DeeplinkWrapper.prototype.getCordovaURIFRomIntent = function(successCallback, failureCallback) {
+    'use strict';
+
+    return cordova.exec (
+        successCallback,
+        failureCallback,
+        "DeeplinkWrapper",
+        "getUriFromIntent",
+        []
+    );
 };
+
+
+var deepLinks = new DeeplinkWrapper();
+module.exports = deepLinks;
+
+// Make plugin work under window.plugins
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.deepLinks) {
+    window.plugins.deepLinks = deepLinks;
+}
