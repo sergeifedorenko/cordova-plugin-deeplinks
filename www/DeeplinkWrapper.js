9bbac5c2e148b341d10dbb2651ee6515edcb57c7
AKosmachyov <a.kosmachyov@gmail.com>
2018-06-29 17:01:22 +0300

    Updated plugin

--- www/DeeplinkWrapper.js
+++ www/DeeplinkWrapper.js
@@ -2,15 +2,21 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = 'DeeplinkPlugin';
 
 module.exports = {
+    /**
+     * Get deeplink
+     */
     getDeeplink: function() {
         return new Promise(function(resolve, reject) {
             exec (resolve, reject, PLUGIN_NAME, "getDeeplink", []);
         })
     },
-
+  
+    /**
+     * Subscribe to the deeplink event
+     */
     onDeepLink: function(callback) {
         return new Promise(function(resolve) {
-            exec (callback, null, PLUGIN_NAME, "onDeepLink", []);
+            exec (resolve, null, PLUGIN_NAME, "onDeepLink", []);
         }).then(callback)
     }
 };
